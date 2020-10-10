fa887f6d06d361872e7d9ecd405d83af3b1620d5
Siddhesh Shivgan <siddheshshivgan2306@gmail.com>
2020-10-10 09:11:42 +0530

    GPS integration done

--- app/src/main/java/com/example/musicapp/HomeActivity.java
+++ app/src/main/java/com/example/musicapp/HomeActivity.java
@@ -1,13 +1,23 @@
 package com.example.musicapp;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore.Images.Media;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,53 +25,53 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
-public class HomeActivity extends AppCompatActivity implements OnNavigationItemSelectedListener {
+public class HomeActivity extends AppCompatActivity implements OnNavigationItemSelectedListener, LocationListener {
+    private static final int LOCATION_CODE = 1002;
     private static final int IMAGE_CAPTURE_CODE = 1001;
     private static final int PERMISSION_CODE = 1000;
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
+    TextView city,state,country;
     private NavigationView navigationView;
+    LocationManager locationManager;
     Uri image_uri;
     CircleImageView profile;
-//    ListView listView;
-//    String[] listItem;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         toolbar = findViewById(R.id.main_toolbar);
+        state = findViewById(R.id.state);
+//        city = findViewById(R.id.city);
+//        country = findViewById(R.id.country);
         setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         profile = navigationView.inflateHeaderView(R.layout.nav_header).findViewById(R.id.profile);
         profile.setImageResource(R.drawable.ic_baseline_person_24);
-//        listView=(ListView)findViewById(R.id.listView);
-//        listItem = getResources().getStringArray(R.array.playlist);
-//        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-//                android.R.layout.simple_list_item_1, android.R.id.text1, listItem);
-//        listView.setAdapter(adapter);
-//
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-//                String value=adapter.getItem(position);
-//                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
+        grantPermission();
+        checkLocEnableOrNot(); //direct to GPS settings
+        getLocation();
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
                 this,
                 drawerLayout,
@@ -83,6 +93,50 @@ public class HomeActivity extends AppCompatActivity implements OnNavigationItemS
                 }
             }
         });
+    }
+
+    private void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,100,5,(LocationListener) this);
+        }catch (SecurityException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void checkLocEnableOrNot() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsEnabled= false;
+        boolean networkEnabled = false;
+        try {
+            gpsEnabled=lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            networkEnabled =lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        if(!gpsEnabled && !networkEnabled){
+            new AlertDialog.Builder(HomeActivity.this)
+                    .setTitle("Enable GPS Service")
+                    .setCancelable(false)
+                    .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    }).setNegativeButton("Cancel",null).show();
+
+        }
+    }
+
+    private void grantPermission() {
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},LOCATION_CODE);
+        }
     }
 
     private void cameraIntent() {
@@ -125,4 +179,34 @@ public class HomeActivity extends AppCompatActivity implements OnNavigationItemS
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        try {
+            Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+//            city.setText(addresses.get(0).getLocality());
+                state.setText(addresses.get(0).getAdminArea());
+//            country.setText(addresses.get(0).getCountryName());
+//            tvPin.setText(addresses.get(0).getPostalCode());
+//            tvLocality.setText(addresses.get(0).getAddressLine(0));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
